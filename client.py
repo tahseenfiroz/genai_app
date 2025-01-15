4f96f5a0ac4313ca7e62a443effc3f8da69aac92
tahseenfiroz <tahseenfiroz1112@gmail.com>
2025-01-15 12:19:35 -0500

    testing

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 from src.model import embedding
 from src.data import data_raw
 from src.database import shared
-import streamlit as st
+# import streamlit as st
 
 df_raw = data_raw.data_raw()
 # df_raw['question_embedding'] = df_raw['question]'.apply(embedding.get_embeddings)
@@ -10,11 +10,11 @@ df_raw['answer_embedding'] = df_raw['answer'].apply(embedding.get_embeddings)
 shared.pgv_truncate('app_semantic','player')
 shared.pgv_insert(df_raw,'app_semantic','player')
 df = shared.pgv_semantic_search('who is Kohli', 'app_semantic', 'player', 'question_embedding')
-
-# Initialize st.session_state.beans
-st.session_state.beans = st.session_state.get("beans", 0)
-
-st.title("Bean counter :paw_prints:")
+print(1)
+# # Initialize st.session_state.beans
+# st.session_state.beans = st.session_state.get("beans", 0)
+#
+# st.title("Bean counter :paw_prints:")
 
 
 
