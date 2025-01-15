db162d978df0e1ee0661aa6aa32a4c7a35e269cf
tahseenfiroz <tahseenfiroz1112@gmail.com>
2025-01-15 12:26:49 -0500

    test

--- client.py
+++ client.py
@@ -11,20 +11,3 @@ shared.pgv_truncate('app_semantic','player')
 shared.pgv_insert(df_raw,'app_semantic','player')
 df = shared.pgv_semantic_search('who is Kohli', 'app_semantic', 'player', 'question_embedding')
 print(1)
-# # Initialize st.session_state.beans
-# st.session_state.beans = st.session_state.get("beans", 0)
-#
-# st.title("Bean counter :paw_prints:")
-
-
-
-
-addend = st.number_input("Beans to add", 0, 10)
-if st.button("Add"):
-
-
-    st.session_state.beans += addend
-st.markdown(f"Beans counted: {st.session_state.beans}")
-
-print(1)
-
